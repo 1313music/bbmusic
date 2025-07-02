266caa9ec49d18011da3a403f3b3a8ede3131be2
1313music <minyaoclub2@163.com>
2025-07-02 23:03:54 +0800

    Add files via upload

---
+++ HeoMusic/config.js
@@ -0,0 +1,19 @@
+var userId = "8865774369";
+var userServer = "netease";
+var userType = "playlist";
+// var localMusic = [{
+//     name: '和你在一起',
+//     artist: '三缺一东京',
+//     url: '/music//05_和你在一起_2024_In_Tokyo_live_.mp3',
+//     cover: '/img/tokyo.jpg',
+//     lrc: '/music/重生之我在异乡为异客.lrc'
+// },
+// {
+//     name: '落',
+//     artist: '唐伯虎',
+//     url: '/music/落.mp3',
+//     cover: '/music/落.png',
+//     lrc: '/music/落.lrc'
+// }
+// ];
+// var remoteMusic = "./musiclist.json"
