ce19670a913034a74991640e6713ae21bd3ececa
1313music <minyaoclub2@163.com>
2025-10-28 16:48:16 +0800

    Add files via upload

---
+++ js/other.js
@@ -0,0 +1,7 @@
+// 其他专辑歌曲列表
+window.otherSongs = [
+  { name: "忽然(i18版)", src: "https://r2.1701701.xyz/other/忽然(i18版) - 李志.mp3", lrc: "" },
+  { name: "李志_榕树下 民谣在路上_梵高先生", src: "https://r2.1701701.xyz/other/李志_榕树下 民谣在路上_梵高先生.mp3", lrc: "" },
+  { name: "这个世界会好吗2015", src: "https://r2.1701701.xyz/other/这个世界会好吗2015.mp3", lrc: "" },
+  { name: "酒吧叙事体 - 肖晨&陈小二", src: "https://r2.1701701.xyz/other/酒吧叙事体 - 肖晨&陈小二.mp3", lrc: "" }
+];
